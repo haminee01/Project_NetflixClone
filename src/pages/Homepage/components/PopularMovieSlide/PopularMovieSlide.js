23541f818c513410b11d9ae1c814b345d6208a9d
haminee01 <146091591+haminee01@users.noreply.github.com>
2024-02-20 22:51:33 +0900

    sc

--- src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.js
+++ src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.js
@@ -1,26 +1,9 @@
 import React from "react";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
 import ClipLoader from "react-spinners/ClipLoader";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import { Alert } from "bootstrap";
-import MovieCard from "../MovieCard/MovieCard";
-import "./PopularMovieSlide.style.css";
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 5,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
-};
+import MovieSlider from "../../../../common/MovieSlider/MovieSlider";
+import { responsive } from "../../../../constants/responsive";
 
 const PopularMovieSlide = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
@@ -37,20 +20,11 @@ const PopularMovieSlide = () => {
   }
 
   return (
-    <div className="popular-slide">
-      <h3>Popular Movies</h3>
-      <Carousel
-        infinite={true}
-        centerMode={true}
-        itemClass="movie-slider p-1"
-        containerClass="carousel-container"
-        responsive={responsive}
-      >
-        {data.results.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </Carousel>
-    </div>
+    <MovieSlider
+      title="Popular Movies"
+      movies={data.results}
+      responsive={responsive}
+    />
   );
 };
 
