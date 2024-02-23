9402b7134f3ff445360990ea39da8bfdf48f0b7c
haminee01 <146091591+haminee01@users.noreply.github.com>
2024-02-23 20:16:51 +0900

    moviePage

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 const AppLayout = () => {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+
+  const searchByKeyword = (event) => {
+    event.preventDefault();
+    //url 바꿔주기
+    navigate(`/movies?q=${keyword}`);
+    setKeyword("");
+  };
+
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
@@ -29,14 +39,18 @@ const AppLayout = () => {
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/movies">Movies</Nav.Link>
             </Nav>
-            <Form className="d-flex">
+            <Form className="d-flex" onSubmit={searchByKeyword}>
               <Form.Control
                 type="search"
                 placeholder="Search"
                 className="me-2"
                 aria-label="Search"
+                value={keyword}
+                onChange={(event) => setKeyword(event.target.value)}
               />
-              <Button variant="outline-danger">Search</Button>
+              <Button variant="outline-danger" type="submit">
+                Search
+              </Button>
             </Form>
           </Navbar.Collapse>
         </Container>
--- src/pages/Movies/MoviePage.js
+++ src/pages/Movies/MoviePage.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
+import { useSearchParams } from "react-router-dom";
+import ClipLoader from "react-spinners/ClipLoader";
+import Alert from "react-bootstrap/Alert";
+import { Col, Container, Row } from "react-bootstrap";
+import MovieCard from "../../common/MovieCard/MovieCard";
 
 const MoviePage = () => {
-  return <div>MoviePage</div>;
+  const [query, setQuery] = useSearchParams();
+  const keyword = query.get("q");
+
+  const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword });
+  if (isLoading) {
+    return (
+      <div className="spinner">
+        <ClipLoader color="#ffff" loading={isLoading} size={150} />
+      </div>
+    );
+  }
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col lg={4} xs={12}>
+          {" "}
+          필터{" "}
+        </Col>
+        <Col lg={8} xs={12}>
+          <Row>
+            {data?.results.map((movie, index) => (
+              <Col key={index} lg={4} xs={12}>
+                <MovieCard movie={movie} />
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default MoviePage;
