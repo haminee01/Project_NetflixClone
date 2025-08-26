f74175110c0157445bcc2e519f6aa062d68f6165
haminee01 <146091591+haminee01@users.noreply.github.com>
2025-08-26 20:01:09 +0900

    ts2

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 const queryClient = new QueryClient();
 root.render(
   <QueryClientProvider client={queryClient}>
